f0ba1806d7c61e480c561d8eda31db2805c63ef2
roberto <robpalacios1@hotmail.com>
2020-09-06 14:41:48 -0500

    add styles component into CharacterImageStyled

--- src/Character-name.js
+++ src/Character-name.js
@@ -2,13 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const CharacterNameStyled = styled.div `
-
+    display: flex;
+    align-items: center;
+    grid-area: character-name;
 `
 
 function CharacterName({ name }) {
     return (
         <CharacterNameStyled>
-            {name}
+            <div class="character-name">
+                <h2>{name}</h2>
+            </div>
         </CharacterNameStyled>
     )
 }
