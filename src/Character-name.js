aefc1c287056349134ded31f76c4aca8ae7ea728
roberto <robpalacios1@hotmail.com>
2020-09-06 14:58:13 -0500

    add more styled component into CharacterNameStyled

--- src/Character-name.js
+++ src/Character-name.js
@@ -5,12 +5,37 @@ const CharacterNameStyled = styled.div `
     display: flex;
     align-items: center;
     grid-area: character-name;
+    .name {
+        position: relative;
+        font-size: 30px;
+    }
+    .name h2:after {
+        content: "";
+        width: 87px;
+        height: 6px;
+        background-color: #F7391C;
+        display: inline-flex;
+        position: absolute;
+        left: calc(100% + 5px);
+        top: 50%;
+    }
+    @media screen and (max-width: 1024px) {
+        margin-top: 1em;
+        justify-content: center;
+    .name {
+        text-align: center;
+    }
+    .name h2:after {
+        left: 0;
+        top: calc(100% - 20px);
+    }
+}
 `
 
 function CharacterName({ name }) {
     return (
         <CharacterNameStyled>
-            <div class="character-name">
+            <div className="name">
                 <h2>{name}</h2>
             </div>
         </CharacterNameStyled>
