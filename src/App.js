009a613af4df9168bd30add35ba7c8186562b181
roberto <robpalacios1@hotmail.com>
2020-09-06 18:57:20 -0500

    add code to render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import Character from './Character'
+//import Character from './Character'
 import CharacterName from './Character-name'
 import CharacterImage from './Character-image'
 import CharacterDescription from './Character-description'
+import CharacterPlaceHolder from './Character-placeholder'
+import Layout from './Layout'
 import API from './Api'
 
 const api = new API()
@@ -20,57 +22,13 @@ function App() {
     }, [])
   return (
     <div className="App">
-      <Character />
-        <div className="placeholder-container">
-    <h1 id="character-name-placeholder" className="character-name-placeholder"></h1>
-  </div>
-  <div className="grid">
-    <span className="asset top">
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="line"></span>
-    </span>
-    <img className="logo" src="./static/images/logo@2x.png" width="280" alt="" />
-    <div className="social">
-      <a href="https://instagram.com/LeonidasEsteban">
-        <img src="./static/images/instagram.svg" alt="" />
-      </a>
-      <a href="https://facebook.com/LeonidasEsteban">
-        <img src="./static/images/facebook.svg" alt="" />
-      </a>
-      <a href="https://twitter.com/LeonidasEsteban">
-        <img src="./static/images/twitter.svg" alt="" />
-      </a>
-    </div>
-    <div className="navigation name">
-      <a href="#">Name</a>
-    </div>
-    <CharacterName name={character.name} />
-    <CharacterImage image={character.image} name={Character.name} />
-
-
-    <div className="navigation about">
-      <a href="#">About</a>
-    </div>
-
-    <CharacterDescription gender={character.gender} species={character.species} status={character.status}/>
-   
-    <span className="asset bottom">
-      <span className="line"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-      <span className="dot"></span>
-    </span>
-
-    <div></div>
-    <div className="learn-more">
-      <span>learn more</span>
-    </div>
-
-    <div className="arrow" id="load-next"></div>
-
-  </div>
+      {/*<Character />*/}
+      <CharacterPlaceHolder name={character.name}/>
+      <Layout
+        name={<CharacterName name={character.name} />}
+        image={<CharacterImage image={character.image} name={character.name} />}
+        description={<CharacterDescription gender={character.gender} species={character.species} status={character.status}/>}
+      />
     </div>
   );
 }
