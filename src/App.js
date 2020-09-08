37427ebfc39cf73e640970c60ff1a8b08b811a88
roberto <robpalacios1@hotmail.com>
2020-09-07 19:23:25 -0500

    import add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React from 'react';
 import './Nprogress.css';
-//import Character from './Character'
-import CharacterName from './Character-name'
-import CharacterImage from './Character-image'
-import CharacterDescription from './Character-description'
-import CharacterPlaceHolder from './Character-placeholder'
-import Layout from './Layout'
-import Next from './Next'
-import api from './Api'
-import CharacterContext from './Character-context'
 import GlobalStyle from './Global-style'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Character from './Character'
 
 
 function App() {
-  const [character, setCharacter] = useState({})
-    useEffect(() => {
-        async function getCharacter() {
-            setCharacter(await api.getCharacter(1))
-            //const character =
-            //console.log(character)
-        }
-        getCharacter()
-    }, [])
   return (
-    <CharacterContext.Provider value={{
-      character,
-      setCharacter
-    }}>
+    <div>
       <GlobalStyle />
-      <CharacterPlaceHolder name={character.name}/>
-      <Layout
-        next={<Next />}
-        name={<CharacterName name={character.name} />}
-        image={<CharacterImage image={character.image} name={character.name} />}
-        description={<CharacterDescription gender={character.gender} species={character.species} status={character.status}/>}
-      />
-    </CharacterContext.Provider>
-  );
+      <Router>
+        <Switch>
+          <Route path={`${process.env.PUBLIC_URL}/`} exact component={Character}/>
+          <Route path={`${process.env.PUBLIC_URL}/:id`} exact component={Character}/>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
